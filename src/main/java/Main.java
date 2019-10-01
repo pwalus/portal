f352159613e7764ece57b15d03ba2887ad2b62c5
pwalus <patryk.walus@ageno.pl>
2019-10-01 19:26:31 +0200

    Project initialization

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import com.paralleldots.paralleldots.App;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        App pd = new App("8XfjV6fhholhlA8JOQ8zv83L7a7Gh8Ugfqk1B94BRVI");
+        System.out.println("Abuse");
+        String abuse = pd.abuse("you f**king a$$hole");
+
+        System.out.println("lol");
+    }
+}
